a00110c6ebb2589b9505f05801e6afbf021a7981
Eberhard Wolff <eberhard.wolff@gmail.com>
2014-10-24 18:56:08 +0200

    Changed test to automatically allocate port.

--- simplest-spring-boot/src/test/java/com/ewolff/simplestspringboot/WebIntegrationTest.java
+++ simplest-spring-boot/src/test/java/com/ewolff/simplestspringboot/WebIntegrationTest.java
@@ -1,10 +1,10 @@
 package com.ewolff.simplestspringboot;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,15 +13,18 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ControllerAndMain.class)
-@IntegrationTest
 @WebAppConfiguration
+@IntegrationTest("server.port=0")
 public class WebIntegrationTest {
 
+	@Value("${local.server.port}")
+	private int serverPort;
+	
 	@Test
 	public void homePage() {
 		RestTemplate restTemplate = new RestTemplate();
 		assertEquals("hello", restTemplate.getForObject(
-				"http://127.0.0.1:8080", String.class));
+				"http://127.0.0.1:"+serverPort, String.class));
 	}
 
 }
--- spring-boot-ops/src/test/java/com/ewolff/springbootdatajpa/WebIntegrationTest.java
+++ spring-boot-ops/src/test/java/com/ewolff/springbootdatajpa/WebIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,21 +14,25 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ControllerAndMain.class)
-@IntegrationTest
+@IntegrationTest("server.port=0")
 @WebAppConfiguration
 public class WebIntegrationTest {
 
+	@Value("${local.server.port}")
+	private int serverPort;
+
+	
 	@Test
 	public void homePage() {
 		RestTemplate restTemplate = new RestTemplate();
-		assertTrue(restTemplate.getForObject("http://127.0.0.1:8080",
+		assertTrue(restTemplate.getForObject("http://127.0.0.1:"+serverPort,
 				String.class).contains("Wolff"));
 	}
 
 	@Test
 	public void health() {
 		RestTemplate restTemplate = new RestTemplate();
-		assertTrue(restTemplate.getForObject("http://127.0.0.1:8080/health",
+		assertTrue(restTemplate.getForObject("http://127.0.0.1:"+serverPort+"/health",
 				String.class).contains("UP"));
 	}
 
