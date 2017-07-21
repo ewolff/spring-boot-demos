09c835d3d04ef0660589caf8c2f74444e4aa8c33
Eberhard Wolff <eberhard.wolff@gmail.com>
2017-07-21 13:12:24 +0200

    Now uses @SpringBootApplication

--- simplest-spring-boot/src/main/java/com/ewolff/simplestspringboot/ControllerAndMain.java
+++ simplest-spring-boot/src/main/java/com/ewolff/simplestspringboot/ControllerAndMain.java
@@ -1,12 +1,12 @@
 package com.ewolff.simplestspringboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ControllerAndMain {
 
 	@RequestMapping("/")
